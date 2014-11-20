c0f1b325143da7d878a861fa2874c183a4a0b30d
Github-Minghui <wangmih803@gmail.com>
2014-11-20 13:26:54 -0500

    add some annotations to the dookie.html; and few changes to the app.js

--- app.js
+++ app.js
@@ -1,31 +1,25 @@
 (function(){
-  var app = angular.module('bulletinapp', []);
+  var app = angular.module('BookStoreApp', []);
   
-  var k = app.controller('klock', function(){
+  app.controller('TogglePage', function(){
     this.login_page = false;
-    
 	this.toggle = function(){
         this.login_page = !(this.login_page);
     };
-
   });
   
-  var formc = app.controller('form', function(){
+  app.controller('form', function(){
 	this.on = true;
-	
 	this.toggle = function(){
 		this.on = !this.on;
 	}
   });
   
-  var tabs = app.controller('tabs',function(){
-	
+  app.controller('NavTab',function(){
 	this.tab=1;
-	
 	this.setTab = function(t){
 		this.tab=t;
 	};
-	
 	this.isSelected = function(tab){
 		return tab===this.tab;
 	};
